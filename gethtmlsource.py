8fcb7c9c1f91bfd8a63c8d0de46ced4cca7912bc
allenlush <allen.lu.sh@outlook.com>
2022-01-26 23:32:50 +0800

    requesting html source and build soup

--- gethtmlsource.py
+++ gethtmlsource.py
@@ -1,5 +1,18 @@
 #Test py to fetch html source from a web page and create a BeautifulSoup object
+
 from bs4 import BeautifulSoup
 import requests
+
+print ('please input url: ')
+url = input()
+try:
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text)
+
+    print (f'your input is: {url} \n')
+    print (f'type of html is: {type(html)}')
+    print (f'type of soup is: {type(soup)}')
+except:
+    print ('there is errors!')
 
 
